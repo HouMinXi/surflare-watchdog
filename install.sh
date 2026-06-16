2a655d2df2ec7f04b5adb54f06439d4dd6700276
Minxi Hou <houminxi@gmail.com>
2026-06-16 17:17:36 +0800

    remove ineffective force-VPN ip-rule mechanism
    The ip-rule approach (ip rule add to X table 100) does not work with the
    surflare tproxy architecture on N100 because table 100 routes to loopback
    (local default dev lo scope host) for tproxy delivery, not to a VPN
    tunnel interface.  Injecting destination-based rules to table 100 caused
    surflare-proxy CN-bypass connections to be routed to loopback instead of
    WAN, breaking CN direct routing.
    
    Remove the three related functions, two config variables, both call sites,
    routes/force_vpn_ips.txt, and revert install.sh to explicit cp commands.
    The bypass_devices/bypass_macs mechanism (Task 1) is unaffected.
    
    Signed-off-by: Minxi Hou <houminxi@gmail.com>

--- install.sh
+++ install.sh
@@ -68,7 +68,8 @@ else
 fi
 
 mkdir -p /usr/local/share/surflare/routes
-cp "$REPO/routes/"*.txt /usr/local/share/surflare/routes/
+cp "$REPO/routes/cn_ipv4.txt" /usr/local/share/surflare/routes/
+cp "$REPO/routes/cn_ipv6.txt" /usr/local/share/surflare/routes/
 
 # ---------------------------------------------------------------------------
 # Service installation (symlink for systemd; cp+chmod for others)
